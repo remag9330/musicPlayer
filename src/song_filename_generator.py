3540d49be6983f2e1bfb0b9687db0fe93c6c2904
remag9330 <remag9330@users.noreply.github.com>
2022-04-01 23:57:52 +1100

    Better youtu.be link handling

--- src/song_filename_generator.py
+++ src/song_filename_generator.py
@@ -15,6 +15,12 @@ def get_filename(url: str) -> str:
 		if name is not None:
 			return name
 
+	if "youtu.be" in url.lower():
+		vid_id = url.strip("/").split("/")[-1]
+		name = try_get_filename_from_id(vid_id)
+		if name is not None:
+			return name
+
 	logging.info("Attempting to get filename from YTDL")
 	return ytdl.get_filename(url)
 
@@ -26,6 +32,9 @@ def try_get_youtube_filename_quick(url: str) -> Optional[str]:
 		logging.exception(f"Error while getting video ID from supposedly good youtube URL '{url}'")
 		return None
 
+	return try_get_filename_from_id(vid_id)
+
+def try_get_filename_from_id(vid_id: str) -> Optional[str]:
 	logging.info("Attempting to get filename from cached songs")
 	cached = get_youtube_filename_from_cache(vid_id)
 	if cached is not None:
