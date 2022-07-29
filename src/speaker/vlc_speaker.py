fb8afe98f232433b3d8143f03946d645833f83b8
remag9330 <remag9330@users.noreply.github.com>
2022-07-29 11:46:55 +1000

    Updates the VLC speaker to close VLC while paused.
    This is to try and help the issue where leaving it for a few days (potentially paused) seems to cause it to stop playing sound at all when resuming.

--- src/speaker/vlc_speaker.py
+++ src/speaker/vlc_speaker.py
@@ -1,4 +1,5 @@
 import time
+import logging
         
 from typing import Optional, Tuple
 from .abstract_speaker import AbstractSpeaker
@@ -11,6 +12,9 @@ class VlcSpeaker(AbstractSpeaker):
     def __init__(self) -> None:
         self.__track: Mutex[Optional[MediaPlayer]] = Mutex(None)
         self.__volume: float = 1.0
+        self.__current_playing_filename: str = ""
+
+        self.__pause_state: Optional[Tuple[int, str]] = None
 
     def load(self, filename: str) -> None:
         with self.__track.acquire() as track:
@@ -18,6 +22,7 @@ class VlcSpeaker(AbstractSpeaker):
                 track.value.release()
 
             track.replace_value(MediaPlayer(filename))
+            self.__current_playing_filename = filename
 
     def play(self) -> None:
         with self.__track.acquire() as track:
@@ -27,29 +32,54 @@ class VlcSpeaker(AbstractSpeaker):
                 # to wait a split second so we don't end up changing songs after returning from here.
                 time.sleep(0.5)
 
-        self._update_track_volume()
+            self._update_track_volume_locked(track.value)
 
     def pause(self) -> None:
         with self.__track.acquire() as track:
             if track.value:
-                track.value.pause()
+                self.__pause_state = (
+                    self.get_pos_locked(track.value),
+                    self.__current_playing_filename
+                )
+                
+                track.value.release()
+                track.replace_value(None)
 
     def unpause(self) -> None:
-        self.play()
+        if self.__pause_state:
+            [start_time, filename] = self.__pause_state
+            self.__pause_state = None
+            self.load(filename)
+            self.play()
+            self.set_pos(start_time)
+        else:
+            logging.warn("Attempting to unpause but there's no pause state to resume from")
+            self.play()
 
     def get_busy(self) -> bool:
         with self.__track.acquire() as track:
-            if track.value:
-                return track.value.is_playing() == 1
-            else:
-                return False
+            return bool(track.value) and track.value.is_playing() == 1
 
     def get_pos(self) -> int:
+        if self.__pause_state:
+            # Short circuit here to avoid grabbing lock if possible, but should still check this in lock too
+            return self.__pause_state[0]
+
+        with self.__track.acquire() as track:
+            return self.get_pos_locked(track.value)
+
+    def get_pos_locked(self, track: Optional[MediaPlayer]) -> int:
+        if self.__pause_state:
+            return self.__pause_state[0]
+        elif track:
+            return track.get_time()
+        else:
+            return 0
+
+    def set_pos(self, pos: int) -> None:
         with self.__track.acquire() as track:
             if track.value:
-                return track.value.get_time()
-            else:
-                return 0
+                track.value.set_time(pos)
 
     def get_volume(self) -> float:
         return self.__volume   
@@ -60,8 +90,12 @@ class VlcSpeaker(AbstractSpeaker):
 
     def _update_track_volume(self) -> None:
         with self.__track.acquire() as track:
-            if track.value and track.value.is_playing():
-                track.value.audio_set_volume(int(self.__volume * 100))
+            self._update_track_volume_locked(track.value)
+
+    def _update_track_volume_locked(self, track: Optional[MediaPlayer]) -> None:
+        if track and track.is_playing():
+            track.audio_set_volume(int(self.__volume * 100))
+
                 
     def volume_min_max_step(self) -> Tuple[float, float, float]:
         return (0.0, 2.0, 0.1)
